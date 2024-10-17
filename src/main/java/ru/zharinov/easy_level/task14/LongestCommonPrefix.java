18b295b79e2b20d863ff722afde864122914ea84
Ilia Zharinov <19Turboboy90@gmail.com>
2024-10-17 17:41:14 +0300

    Update task14

--- src/main/java/ru/zharinov/easy_level/task14/LongestCommonPrefix.java
+++ src/main/java/ru/zharinov/easy_level/task14/LongestCommonPrefix.java
@@ -25,30 +25,27 @@ Constraints:
 strs[i] consists of only lowercase English letters.
  */
 public class LongestCommonPrefix {
-//    public static void main(String[] args) {
-//        String[] strs = {"flower", "flow", "flight"};
-////        String[] strs = {"dog", "racecar", "car"};
-////        String[] strs = {"a"};
-////        String[] strs = {"ab", "a"};
-//        System.out.println(longestCommonPrefix(strs));
-//    }
-//
-//    public static String longestCommonPrefix(String[] strs) {
-//
-//        if (strs == null || strs.length == 0) return "";
-//        Arrays.sort(strs);
-//        String result = "";
-//        String prefix = strs[0];
-//        for (int i = 1; i < strs.length; i++) {
-//            for (int j = 0; j < prefix.length(); j++) {
-////                if (prefix.charAt(j) != strs[i].charAt(j)) {
-////                    result = result.substring(0, j);
-////                } else {
-////                    break;
-////                }
-//            }
-//        }
-//
-//        return result;
-//    }
+    public static void main(String[] args) {
+        String[] str1 = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(str1)); // Вывод: "fl"
+
+        String[] str2 = {"dog", "racecar", "car"};
+        System.out.println(longestCommonPrefix(str2)); // Вывод: ""
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
 }
